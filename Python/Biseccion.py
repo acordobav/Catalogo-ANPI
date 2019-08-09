1c9830c53f9e007efe53075fc2aaa59a2a571a8f
Arturo Cordoba <arturocv16@gmail.com>
2019-08-08 21:17:09 -0600

    Cambio en la forma de obtener la funcion ingresada por el usuario
    Eliminacion del lambda para utilizar sympify para obtener la funcion.

--- Python/Biseccion.py
+++ Python/Biseccion.py
@@ -1,7 +1,4 @@
-import math
-
-#Declaracion de la funcion que se desea evaluar
-g = lambda x : math.exp(x) - x - 2;
+from sympy import sympify
 
 """
 Funcion principal que implementa el metodo de la biseccion para encontrar el cero de una funcion matematica
@@ -14,7 +11,10 @@ Funcion principal que implementa el metodo de la biseccion para encontrar el cer
 def biseccion(a, b, tol, f):
     #Se hace un llamado a una funcion auxiliar que lleva la cuenta del numero de iteraciones totales
     Xaprox = (a + b) / 2;
-    return biseccionAux(a, b, tol, f, Xaprox, 0)
+
+    # Se obtiene la funcion a partir del string ingresado por el usuario
+    funcion = sympify(f)
+    return biseccionAux(a, b, tol, funcion, Xaprox, 0)
 
 
 """
@@ -22,18 +22,18 @@ Funcion auxiliar que implementa el metodo de la biseccion para encontrar el cero
 :param a: limite izquierdo del intervalo en el eje x
 :param b: limite derecho de la funcion en el eje x
 :param tol: tolerancia al fallo que debe tener el resultado final
-:param f: funcion que se desea biseccionar
+:param funcion: funcion que se desea biseccionar
 :param Xaprox: xk calculado en iteraciones anteriores de la funcion
 :param itr: numero total de iteraciones
 :returns: lista con dos elementos, el primero corresponde a el Xaprox obtenido, y el segundo al numero de iteraciones
 """
-def biseccionAux(a, b, tol, f, Xaprox, itr):    
+def biseccionAux(a, b, tol, funcion, Xaprox, itr):
     #Se verifica si el Xaprox cumple la condicion de parada
-    if(abs(f(Xaprox)) <= tol):
+    if(abs(funcion.subs({'x':Xaprox})) <= tol):
         return [Xaprox, itr];
 
-    #Se almacena el resultado de la multiplicacion f(a) * f(b) en una variable
-    multi = f(a) * f(b);
+    #Se almacena el resultado de la multiplicacion funcion(a) * funcion(b) en una variable
+    multi = funcion.subs({'x':a}) * funcion.subs({'x':b});
     
     #Se verifica si se cumple el Teorema de Bolzano
     if(multi <= 0):
@@ -41,20 +41,23 @@ def biseccionAux(a, b, tol, f, Xaprox, itr):
         xk = (a + b) / 2;
 
         #Se verifica si el intervalo de [a, xk] cumple con el Teorema de Bolzano;
-        multiIntervalo1 = f(a) * f(xk);
+        multiIntervalo1 = funcion.subs({'x':a}) * funcion.subs({'x':xk});
         if(multiIntervalo1 <= 0):
             #Si se cumple la condicion se hace un llamado recursivo utilizando xk como
             #nuevo limite derecho del intervalo
-            return biseccionAux(a, xk, tol, f, xk, itr + 1);
+            return biseccionAux(a, xk, tol, funcion, xk, itr + 1);
 
         #En caso de que el intervalo1 no cumpla con el TB, se verifica si el intervalo2 lo cumple
-        multiIntervalo2 = f(xk) * f(b); 
+        multiIntervalo2 = funcion.subs({'x':xk}) * funcion.subs({'x':b});
         if(multiIntervalo2 <= 0):
             #Si se cumple la condicion se hace un llamado recursivo utilizando xk como
             #nuevo limite izquierdo del intervalo
-            return biseccionAux(xk, b, tol, f, xk, itr + 1);
+            return biseccionAux(xk, b, tol, funcion, xk, itr + 1);
         
 
     #Si la funcion no cumple el Teorema de Bolzano en el intervalo ingresado entonces
     #se le indica al usuario
     return "La funcion no cumple con el Teorema de Bolzano. Iteraciones: 0";
+
+#f = 'exp(x) - x - 2'
+#biseccion(0, 2, 10**-5, f)
