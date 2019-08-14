73d1a8b918bb82a0e3068aa91daea28ce0819875
Arturo Cordoba <arturocv16@gmail.com>
2019-08-14 14:46:29 -0600

    Cambio en los comentarios Falsa Posicion Octave

--- Octave/falsa_posicion.m
+++ Octave/falsa_posicion.m
@@ -6,55 +6,57 @@ Funcion que se encarga de calcular el xk+1 utilizando el metodo de la secante
 :param xk: xk de la iteracion anterior
 :param ck: limite izquierdo del intervalo si se seleeciona [ak, xk],
            limite derecho del intervalo si se selecciona [xk, ck]
+:returns: se retorna el xk+1 calculado
 %}
-function [xk1] = xkSecante(funcion, xk, ck)
-    %Se calcula el xk+1 utilizando el metodo de la secante
+function [xk1] = xk_secante(funcion, xk, ck)
+    % Se calcula el xk+1 utilizando el metodo de la secante
     xk1 = xk - (funcion(xk) * (xk - ck)) / (funcion(xk) - funcion(ck));
-end % xkSecante(funcion, xk, ck)
+end % xk_secante(funcion, xk, ck)
 
 %{
-Funcion que implementa el metodo de la falsa posicion para encontrar el cero de una funcion
+Metodo de  falsa posicion para encontrar el cero de una funcion
 :param f: funcion que se debe evaluar
 :param a: limite izquierdo del intervalo
 :param b: limite derecho del intervalo
-:param tol: tolerancia al fallo que debe tener el resultado, debe ser un numero que se encuentre entre cero y uno
+:param tol: tolerancia al fallo del resultado, numero entre cero y uno
+:returns: lista con dos elementos, x_aprox obtenido y numero de iteraciones
 %}
-function [xAprox, iter] = falsa_posicion(f, a, b, tol)
-    funcion = matlabFunction(sym(f)); %Se obtiene la funcion ingresada por el usuario
-    itr = 0; %Se inicializa el contador
-    xk = xkSecante(funcion, b, a); %Se calcula el xk inicial
+function [x_aprox, iter] = falsa_posicion(f, a, b, tol)
+    funcion = matlabFunction(sym(f)); % Se obtiene la funcion ingresada por el usuario
+    itr = 0; % Se inicializa el contador de iteraciones
+    xk = xk_secante(funcion, b, a); % Se calcula el xk inicial
 
-    intervalo0 = funcion(a) * funcion(b); %Se verifica si en el intervalo existe un cero
+    intervalo0 = funcion(a) * funcion(b); % Se verifica si en el intervalo existe un cero
     if ~(intervalo0 < 0)
-        xAprox = "La funcion no garantiza la existencia de un cero en el intervalo ingresado";
+        x_aprox = "La funcion no garantiza la existencia de un cero en el intervalo ingresado";
         iter = 0;
     end % ~(intervalo0 < 0)
 
     while 1
-        %Se verifica la condicion de parada
+        % Se verifica la condicion de parada
         if abs(funcion(xk)) <= tol
             break;
         end % abs(funcion(xk)) <= tol
 
-        %Se verifica si en el primer intervalo se garantiza la existencia de un cero
+        % Verifica si primer intervalo garantiza la existencia de un cero
         intervalo1 = funcion(a) * funcion(xk);
         if intervalo1 < 0
             b = xk; %Se define el nuevo limite derecho
             itr = itr + 1; %Se aumenta el contador de iteraciones
-            xk = xkSecante(funcion, xk, a); %Se calcula el nuevo xk
+            xk = xk_secante(funcion, xk, a); %Se calcula el nuevo xk
 
         else
             a = xk; %Se define el nuevo limite izquierdo
             itr = itr + 1; %Se aumenta el contador de iteraciones
-            xk = xkSecante(funcion, xk, b); %Se calcula el nuevo xk
+            xk = xk_secante(funcion, xk, b); %Se calcula el nuevo xk
         end % intervalo1 < 0
     end % while 1
 
         %Se retornan los valores obtenidos
-        xAprox = xk;
+        x_aprox = xk;
         iter = itr;
 
 end %falsa_posicion(f, a, b, tol)
 
-%[xAprox, iter] = falsa_posicion('cos(x)-x', 1/2, pi, 10^-5)
-%[xAprox, iter] = falsa_posicion('exp(2*x) - 10 - log(x/2)', 1, 1.2, 10^-2)
+% [xAprox, iter] = falsa_posicion('cos(x)-x', 1/2, pi, 10^-5)
+% [xAprox, iter] = falsa_posicion('exp(2*x) - 10 - log(x/2)', 1, 1.2, 10^-2)
