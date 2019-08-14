feb384da618ce319e9efc671389f0c67a16fd554
Arturo Cordoba <arturocv16@gmail.com>
2019-08-14 15:35:48 -0600

    Cambio en los comentarios, Secante Octave

--- Octave/secante.m
+++ Octave/secante.m
@@ -1,40 +1,41 @@
 pkg load symbolic
 
 %{
-Funcion que implementa el metodo de la secante para encontrar el cero de una funcion
+Funcion que implementa el metodo de la secante
 :param f: string con la funcion que se debe evaluar
-:param xKAnterior2: valor de xk de la iteracion inicial
-:param xkAnterior1: valor de xk de la segunda iteracion
+:param xk_anterior2: valor de xk de la iteracion inicial
+:param xk_anterior1: valor de xk de la segunda iteracion
 :param tol: tolerancia al fallo que debe cumplir el resultado
+:returns: lista con dos elementos, xk calculado y numero iteraciones
 %}
-function [xAprox, iter] = secante(f, xkAnterior2, xkAnterior1, tol)
-    funcion = matlabFunction(sym(f)); %Se obtiene la funcion ingresada por el usuario
-    itr = 0 %Se inicializa el contador de iteraciones
+function [x_aprox, iter] = secante(f, xk_anterior2, xk_anterior1, tol)
+    funcion = matlabFunction(sym(f)); % Se obtiene la funcion ingresada por el usuario
+    itr = 0 % Se inicializa el contador del numero de iteraciones
 
-    %Bucle infinito que se rompe al encontrar la condicion de parada
+    % While infinito que se rompe al cumplir la condicion de parada
     while 1
-        %Se calcula el xk de la iteracion actual
-        xk = xkAnterior1 - ((funcion(xkAnterior1)*(xkAnterior1-xkAnterior2))/(funcion(xkAnterior1)-funcion(xkAnterior2)));
+        % Se calcula el xk de la iteracion actual
+        xk = xk_anterior1 - ((funcion(xk_anterior1)*(xk_anterior1-xk_anterior2))/(funcion(xk_anterior1)-funcion(xk_anterior2)));
 
-        %Se evalua la funcion en el valor de xk
+        % Se evalua la funcion en el valor de xk
         fxk = funcion(xk);
 
-        %Se verifica la condicion de parada
+        % Se verifica la condicion de parada
         if(abs(fxk) <= tol)
-            break; %Se rompe el bucle infinito
+            break; % Se rompe el bucle infinito
 
         end % End if(abs(fxk) <= 0)
 
-        %Se ajustan las variables para la siguiente iteracion
-        xkAnterior2 = xkAnterior1;
-        xkAnterior1 = xk;
+        % No cumple condicion de parada, ajuste de variables para sig iteracion
+        xk_anterior2 = xk_anterior1;
+        xk_anterior1 = xk;
         itr = itr + 1;
 
     end % End While
 
-    xAprox = xk;
+    x_aprox = xk;
     iter = itr;
 
-end %secante(f, xkAnterior2, xkAnterior1, tol)
+end % secante(f, xk_anterior2, xk_anterior1, tol)
 
-%[xAprox, iter] = secante('exp(2*x) - 10 - log(x/2)', 1, 1.2, 10^-2)
+% [xAprox, iter] = secante('exp(2*x) - 10 - log(x/2)', 1, 1.2, 10^-2)
