dd0071bd89f15d6416175b6519865e5c13632112
Arturo Cordoba <arturocv16@gmail.com>
2019-08-14 14:16:03 -0600

    Cambio en los comentarios, falsa posicion python

--- Python/falsa_posicion.py
+++ Python/falsa_posicion.py
@@ -7,54 +7,62 @@ Funcion que se encarga de calcular el xk+1 utilizando el metodo de la secante
 :param xk: xk de la iteracion anterior
 :param ck: limite izquierdo del intervalo si se seleeciona [ak, xk],
            limite derecho del intervalo si se selecciona [xk, ck]
+:returns: se retorna el xk+1 calculado
 """
-def xkSecante(funcion, xk, ck):
+
+
+def xk_secante(funcion, xk, ck):
     # Se calcula el xk+1 utilizando el metodo de la secante
     xk1 = float(xk - (funcion.subs({'x': xk}) * (xk - ck)) / (
-                funcion.subs({'x': xk}) - funcion.subs({'x': ck})));
+            funcion.subs({'x': xk}) - funcion.subs({'x': ck})))
     return xk1
 
+
 """
-Funcion que implementa el metodo de la falsa posicion para encontrar el cero de una funcion
+Metodo de  falsa posicion para encontrar el cero de una funcion
 :param f: funcion que se debe evaluar
 :param a: limite izquierdo del intervalo
 :param b: limite derecho del intervalo
-:param tol: tolerancia al fallo que debe tener el resultado, debe ser un numero que se encuentre entre cero y uno
+:param tol: tolerancia al fallo del resultado, numero entre cero y uno
+:returns: lista con dos elementos, x_aprox obtenido y numero de iteraciones
 """
-def falsa_posicion(f, a, b, tol):
-    funcion = sympify(f); #Se obtiene la funcion ingresada por el usuario
-    iter = 0; #Se inicializa el contador del numero de iteraciones
-    xk = xkSecante(funcion, b, a); #Se calcula el valor inicial de xk
 
-    #Se verifica si en el intervalo ingresado existe un cero
-    intervalo0 = float(funcion.subs({'x':a}) * funcion.subs({'x':b}));
+
+def falsa_posicion(f, a, b, tol):
+    funcion = sympify(f)  # Se obtiene la funcion ingresada por el usuario
+    itr = 0  # Se inicializa el contador del numero de iteraciones
+    xk = xk_secante(funcion, b, a)  # Se calcula el valor inicial de xk
+
+    # Se verifica si en el intervalo ingresado existe un cero
+    intervalo0 = float(funcion.subs({'x': a}) * funcion.subs({'x': b}))
     if not (intervalo0 < 0):
-        return ["La funcion no cumple con el criterio para aplicar este metodo", 0];
+        return ["Funcion no cumple con el criterio para este metodo", 0]
 
     while True:
-        #Se evalua la funcion en el valor de xk
-        fxk = float(funcion.subs({'x':xk}));
+        # Se evalua la funcion en el valor de xk
+        fxk = float(funcion.subs({'x': xk}))
 
-        #Se verifica la condicion de parada
+        # Se verifica la condicion de parada
         if abs(fxk) <= tol:
-            break; #Se termina el ciclo infinito
+            break  # Se termina el ciclo infinito
 
-        #Se verifica si en el primer intervalo garantiza la existencia de un cero
-        intervalo1 = funcion.subs({'x':a}) * funcion.subs({'x':xk});
+        # Verifica si primer intervalo garantiza la existencia de un cero
+        intervalo1 = funcion.subs({'x': a}) * funcion.subs({'x': xk})
         if intervalo1 < 0:
-            b = xk; #Se define el nuevo limite derecho
-            iter += 1; #Se aumenta el contador
-            xk = xkSecante(funcion, xk, a);
+            b = xk  # Se define el nuevo limite derecho
+            itr += 1  # Se aumenta el contador
+            xk = xk_secante(funcion, xk, a)
 
         else:
-            # Se verifica si en el segundo intervalo garantiza la existencia de un cero
-            intervalo2 = funcion.subs({'x':xk}) * funcion.subs({'x':b});
+            # Verifica si segundo intervalo garantiza la existencia de un cero
+            intervalo2 = funcion.subs({'x': xk}) * funcion.subs({'x': b})
             if intervalo2 < 0:
-                a = xk; #Se define el nuevo limite izquierdo
-                iter += 1; #Se aumenta el contador
-                xk = xkSecante(funcion, xk, b);
+                a = xk  # Se define el nuevo limite izquierdo
+                itr += 1  # Se aumenta el contador
+                xk = xk_secante(funcion, xk, b)
 
-    return [xk, iter];
+    return [xk, itr]
+
 
 # funcion = 'cos(x)-x'
-# print(falsa_posicion(funcion, 1/2, math.pi/4, 10**(-5)));
+# print(falsa_posicion(funcion, 1 / 2, math.pi / 4, 10 ** (-5)));
