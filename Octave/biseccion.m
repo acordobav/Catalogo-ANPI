0c2e8091eb289c0d366d96480ff70ae7d77955cf
Arturo Cordoba <arturocv16@gmail.com>
2019-08-14 22:36:15 -0600

    Refactor en el codigo del metodo Biseccion
    Paso de recursivo a iterativo

--- Octave/biseccion.m
+++ Octave/biseccion.m
@@ -8,60 +8,49 @@ Metodo de la biseccion para encontrar el cero de una funcion matematica
 :param f: funcion que se desea biseccionar
 :returns: lista con dos elementos, x_aprox obtenido y numero de iteraciones
 %}
-function [Xaprox, iter] = biseccion(a, b, tol, f)
-    xk = (a + b) / 2;  % Calculo del x_aprox inicial
-    [Xaprox, iter] = biseccion_aux(a, b, tol, f, xk, 0);
-end
-
-%{
-Metodo auxiliar del metodo de la biseccion
-:param a: limite izquierdo del intervalo en el eje x
-:param b: limite derecho de la funcion en el eje x
-:param tol: tolerancia al fallo que debe tener el resultado final
-:param funcion: funcion que se desea biseccionar
-:param x_anterior: xk calculado en iteraciones anteriores de la funcion
-:param itr: numero total de iteraciones
-:returns: lista con dos elementos, x_aprox obtenido y numero de iteraciones
-%}
-function [x_aprox, iter] = biseccion_aux(a, b, tol, f, x_anterior, itr)
+function [x_aprox, iter] = biseccion(a, b, tol, f)
     % Se obtiene la funcion a partir del string
     funcion = matlabFunction(sym(f));
 
-    % Se verifica la condicion de parada del algoritmo
-    if abs(funcion(x_anterior)) <= tol
-        x_aprox = x_anterior;
-        iter = itr;
+    if ~(funcion(a) * funcion(b) <= 0)
+        x_aprox = 'La funcion no cumple con el Teorema de Bolzano.';
+        iter = 0;
+    end % ~(funcion(a) * funcion(b) <= 0)
 
-    else
+    itr = 1;
+
+    while 1
+        xk = (a + b) / 2; % Calculo del x_aprox
+
+        % Se verifica la condicion de parada del algoritmo
+        if abs(funcion(xk)) <= tol
+            break;
+        end
+
         % Se verifica si se cumple el Teorema de Bolzano
-        multi = funcion(a) * funcion(b);
-        if multi <= 0
-            % Se calcula xk, para dividir el intervalo en dos
-            xk = (a + b) / 2;
+        if funcion(a) * funcion(b) <= 0
+            itr = itr + 1;
 
             % Se verifica la condicion en el intervalo de [a, xk]
-            multiIntervalo1 = funcion(a) * funcion(xk);
-            if multiIntervalo1 <= 0
-                [x_aprox, iter] = biseccion_aux(a, xk, tol, f, xk, itr+1);
-            end
+            if funcion(a) * funcion(xk) <= 0
+                b = xk;
 
-            % Intervalo1 no cumple, se verifica si el intervalo2 lo cumple
-            multiIntervalo2 = funcion(xk) * funcion(b);
-            if multiIntervalo2 <= 0
-                [x_aprox, iter] = biseccion_aux(xk, b, tol, f, xk, itr+1);
-            end
+            else
+                % Intervalo1 no cumple, se verifica si el intervalo2 lo cumple
+                if funcion(xk) * funcion(b) <= 0
+                    a = xk;
+                end % funcion(xk) * funcion(b) <= 0
 
-        else
-            % Funcion no cumple el Teorema de Bolzano en el intervalo ingresado
-            x_aprox = 'La funcion no cumple con el Teorema de Bolzano.';
-            iter = 0;
-        end % End multi <= 0
+            end % funcion(a) * funcion(xk) <= 0
 
-    end % if abs(funcion(x_anterior)) <= tol
+        end % funcion(a) * funcion(b) <= 0
+
+    end % while 1
+
+    x_aprox = xk;
+    iter = itr;
+
 end % End biseccion_aux
 
-
-%{
-str = 'exp(x) - x - 2';
-[a, b] = biseccion(0, 2, 0.24034295746184142, str)
-%}
+f = 'exp(x) - x - 2';
+[x_aprox, iter] = biseccion(0, 2, 10^-5 , f)
--- Python/biseccion.py
+++ Python/biseccion.py
@@ -11,50 +11,41 @@ Metodo de la biseccion para encontrar el cero de una funcion matematica
 
 
 def biseccion(a, b, tol, f):
-    x_aprox = (a + b) / 2  # Calculo del x_aprox inicial
     funcion = sympify(f)  # Se obtiene la funcion a partir del string
-    return biseccion_aux(a, b, tol, funcion, x_aprox, 0)
 
+    # Se verifica si el intervalo cumple el TB
+    multi0 = float(funcion.subs({'x': a}) * funcion.subs({'x': b}))
+    if not multi0 <= 0:
+        return ["La funcion no cumple con el Teorema de Bolzano", 0]
 
-"""
-Metodo auxiliar del metodo de la biseccion
-:param a: limite izquierdo del intervalo en el eje x
-:param b: limite derecho de la funcion en el eje x
-:param tol: tolerancia al fallo que debe tener el resultado final
-:param funcion: funcion que se desea biseccionar
-:param x_aprox: xk calculado en iteraciones anteriores de la funcion
-:param itr: numero total de iteraciones
-:returns: lista con dos elementos, x_aprox obtenido y numero de iteraciones
-"""
+    itr = 1
 
+    while True:
+        x_aprox = (a + b) / 2  # Calculo del x_aprox
 
-def biseccion_aux(a, b, tol, funcion, x_aprox, itr):
-    # Se verifica si el x_aprox cumple la condicion de parada
-    if abs(funcion.subs({'x': x_aprox})) <= tol:
-        return [x_aprox, itr]
+        # Se verifica si el x_aprox cumple la condicion de parada
+        if abs(funcion.subs({'x': x_aprox})) <= tol:
+            break
 
-    # Se almacena el resultado de la multiplicacion en una variable
-    multi = funcion.subs({'x': a}) * funcion.subs({'x': b})
+        # Se almacena el resultado de la multiplicacion en una variable
+        multi = funcion.subs({'x': a}) * funcion.subs({'x': b})
 
-    # Se verifica si se cumple el Teorema de Bolzano
-    if multi <= 0:
-        # Se calcula xk, para dividir el intervalo en dos
-        xk = (a + b) / 2
+        # Se verifica si se cumple el Teorema de Bolzano
+        if multi <= 0:
+            itr += 1  # Se aumenta el contador de iteraciones
 
-        # Se verifica la condicion en el intervalo de [a, xk]
-        multi_intervalo1 = funcion.subs({'x': a}) * funcion.subs({'x': xk})
-        if multi_intervalo1 <= 0:
-            # Llamado recursivo utilizando xk como limite derecho del intervalo
-            return biseccion_aux(a, xk, tol, funcion, xk, itr + 1)
+            # Se verifica la condicion en el intervalo de [a, xk]
+            multi_intervalo1 = funcion.subs({'x': a}) * funcion.subs({'x': x_aprox})
+            if multi_intervalo1 <= 0:
+                b = x_aprox
 
-        # Intervalo1 no cumple, se verifica si el intervalo2 lo cumple
-        multi_intervalo2 = funcion.subs({'x': xk}) * funcion.subs({'x': b})
-        if multi_intervalo2 <= 0:
-            # Llamado recursivo, se usa xk como limite izquierdo del intervalo
-            return biseccion_aux(xk, b, tol, funcion, xk, itr + 1)
+            else:
+                # Intervalo1 no cumple, se verifica si el intervalo2 lo cumple
+                multi_intervalo2 = funcion.subs({'x': x_aprox}) * funcion.subs({'x': b})
+                if multi_intervalo2 <= 0:
+                    a = x_aprox
 
-    # Funcion no cumple el Teorema de Bolzano en el intervalo ingresado
-    return "La funcion no cumple con el Teorema de Bolzano. Iteraciones: 0"
+    return [x_aprox, itr]
 
 # f = 'exp(x) - x - 2'
 # biseccion(0, 2, 10**-5, f)
