2059ae5c5f6a0c2bf7808845ddc79a54f7b43933
Arturo Cordoba <arturocv16@gmail.com>
2019-09-15 17:19:35 -0600

    Refactor para utilizar las funciones de sustitucion.py

--- s3_sol_sist_ecuaciones_lineales/metodos_directos/factorizacion_lu/factorizacion_lu.py
+++ s3_sol_sist_ecuaciones_lineales/metodos_directos/factorizacion_lu/factorizacion_lu.py
@@ -1,5 +1,6 @@
-from sympy import Symbol, solve
 from scipy import linalg
+from s3_sol_sist_ecuaciones_lineales.metodos_directos.sustitucion import sustitucion_adelante
+from s3_sol_sist_ecuaciones_lineales.metodos_directos.sustitucion import sustitucion_atras
 
 
 def factorizacion_lu(matriz_a, matriz_b):
@@ -16,42 +17,16 @@ def factorizacion_lu(matriz_a, matriz_b):
     if n != m:
         return "matriz_a debe ser cuadrada"
 
-    # Se crea una lista con las variables simbolicas de la ecuacion
-    for i in range(0, n):
-        lista_simb.append(Symbol('x' + str(i)))
-
     # Se calculan las matrices L y U del sistema
     matriz_i, matriz_l, matriz_u = linalg.lu(matriz_a)
 
     # Se resuelve el sistema L y = b utilizando una sustitucion hacia adelante
-    lista_y = lista_simb.copy()
-    for i in range(0, n):
-        sub_lista1 = matriz_l[i]
-        ecuacion1 = '- ' + str(matriz_b[i][0])
-
-        # Se forma la ecuacion
-        for x in range(0, m):
-            ecuacion1 += ' + ' + str(sub_lista1[x]) + ' * ' + str(lista_y[x])
-
-        # Se resuelve la ecuacion
-        resultado1 = solve(ecuacion1)
-        lista_y[i] = resultado1[0]
+    matriz_y = sustitucion_adelante(matriz_l, matriz_b)
 
     # Se resuelve el sistema U x = y utilizando una sustitucion hacia atras
-    lista_x = lista_simb.copy()
-    for i in range(1, n + 1):
-        sub_lista2 = matriz_u[-i]
-        ecuacion2 = '- ' + str(lista_y[-i])
+    matriz_x = sustitucion_atras(matriz_u, matriz_y)
 
-        # Se forma la ecuacion
-        for x in range(0, m):
-            ecuacion2 += ' + ' + str(sub_lista2[x]) + ' * ' + str(lista_x[x])
-
-        # Se despeja la ecuacin
-        resultado2 = solve(ecuacion2)
-        lista_x[-i] = resultado2[0]
-
-    return lista_x
+    return matriz_x
 
 
 a = [[4, -2, 1], [20, -7, 12], [-8, 13, 17]]
